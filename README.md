5d9fc4ade7fda271889ab98f5765a0a12c204ac7
JimboVong <1150469292@qq.com>
2017-09-24 10:02:05 +0800

    Upload via Github

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Random-Seats-Generator
+# Random Seats Generator
+The program written in Visual Basic by our school authority is too slow. Thus I code my own random seats generator using C++.
+## Instructions
+Run `rsg.exe` to generate `seats.txt`. **PLEASE** make sure that there is `namelist.txt` existing under the same path.
+## Sample Output
+><--后　门-->
+廖　　胡　　
+叶　　杨　　梁　　莫　　许　　郑　　林　　王　　
+余　　陈　　余　　文　　汪　　王　　刘　　郭　　
+张　　辛　　叶　　何　　王　　李　　徐　　黄　　
+柯　　肖　　熊　　谢　　段　　杨　　叶　　曾　　
+郭　　高　　林　　李　　卢　　朱　　梁　　张　　
+<--前　门-->
---
+++ Random-Seats-Generator.cpp
@@ -0,0 +1,33 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+const int STU=42; 
+char name[STU][9];
+int flag[STU];
+int main()
+{
+    freopen("namelist.txt","rt",stdin);
+    freopen("seats.txt","wt",stdout);
+    for(int i=0;i<STU;i++)
+        scanf("%s",&name[i]);
+    srand(time(0));
+    memset(flag,0,sizeof(flag));
+    puts("<--后　门-->");
+    for(int i=0;i<STU;)
+    {
+        int j=rand()%STU;
+        if(flag[j]) continue;
+        flag[j]=1;
+        printf("%s",name[j]);
+        for(j=strlen(name[j])/2;j<6;j++)
+            printf("　");
+        if(!((i-1)%8)) puts("");
+        i++;
+    }
+    puts("<--前　门-->");
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
+ 
